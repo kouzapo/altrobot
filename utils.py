267157ca86a5ca73b326324b466ed580834c5508
Apostolos Kouzoukos <kouzoukos97@gmail.com>
2019-12-30 22:22:35 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import json
+
 import pandas as pd
 import pandas_datareader.data as pdr
+
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')
+
+from keras.models import model_from_json
+
+sys.stderr = stderr
+
+def banner():
+    with open('banner.txt', 'r') as f:
+        for i in f:
+            print(i.splitlines()[0])
+
+def load_model(model_name):
+    with open(model_name + '.json') as f:
+        return model_from_json(json.load(f))
 
 def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#'):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))

703842b73862cb0fd60e685ff04193077059954b
kouzapo <poytiolas@gmail.com>
2020-05-25 21:31:11 +0300

    increace decimals in progress bar

--- altrobot/utils.py
+++ altrobot/utils.py
@@ -28,7 +28,7 @@ def save_model(model, name):
     with open(f'keras_models/{name}.json', 'w') as f:
         json.dump(model.to_json(), f)
 
-def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#'):
+def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 2, length = 100, fill = '#'):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     bar = fill * filled_length + ' ' * (length - filled_length)
